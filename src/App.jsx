483db1e6a0c94aa2329a4ffa852a5fd8f227b21e
kimzerovirus <zerovirus96@gmail.com>
2024-08-27 21:15:37 +0900

    tagify test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CustomMixedTags from "./CustomMixedTags";
+import "@yaireo/tagify/dist/tagify.css";
+
+function App() {
+  return (
+    <>
+      <CustomMixedTags />
+    </>
+  );
+}
+
+export default App;
