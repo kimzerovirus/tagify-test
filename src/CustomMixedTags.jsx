21e4c2f22e3fb8a96d77d4a2ab07a06314afd292
kimzerovirus <zerovirus96@gmail.com>
2024-08-29 23:28:52 +0900

    tagify 화이트리스 적용

--- src/CustomMixedTags.jsx
+++ src/CustomMixedTags.jsx
@@ -2,25 +2,32 @@ import React, { useCallback, useState } from "react";
 import { MixedTags } from "@yaireo/tagify/dist/react.tagify";
 
 const settings = {
+  mode: "mix",
   pattern: /@/, // <- must define "patten" in mixed mode
-  //   enforceWhitelist: true, // 화이트리스트에서 허용된 태그만 사용 // <<< 이거 하면 다 안되네..
+  enforceWhitelist: true, // 화이트리스트에서 허용된 태그만 사용 // <<< 이거 하면 다 안되네..
   dropdown: {
-    enabled: 1,
+    enabled: 0,
     position: "text",
+    userInput: true,
+    includeSelectedTags: true,
   },
-  whitelist: [
-    { id: 100, value: "kenny", title: "Kenny McCormick" },
-    { id: 101, value: "cartman", title: "Eric Cartman" },
-    { id: 102, value: "kyle", title: "Kyle Broflovski" },
-    { id: 103, value: "token", title: "Token Black" },
-    { id: 104, value: "jimmy", title: "Jimmy Valmer" },
-    { id: 105, value: "butters", title: "Butters Stotch" },
-    { id: 106, value: "stan", title: "Stan Marsh" },
-    { id: 107, value: "randy", title: "Randy Marsh" },
-    { id: 108, value: "Mr. Garrison", title: "POTUS" },
-    { id: 109, value: "Mr. Mackey", title: "M'Kay" },
-  ],
+  duplicates: true,
+  trim: true,
+  closeOnSelect: true,
 };
+
+const whitelist = [
+  { id: 100, value: "kenny", title: "Kenny McCormick" },
+  { id: 101, value: "cartman", title: "Eric Cartman" },
+  { id: 102, value: "kyle", title: "Kyle Broflovski" },
+  { id: 103, value: "token", title: "Token Black" },
+  { id: 104, value: "jimmy", title: "Jimmy Valmer" },
+  { id: 105, value: "butters", title: "Butters Stotch" },
+  { id: 106, value: "stan", title: "Stan Marsh" },
+  { id: 107, value: "randy", title: "Randy Marsh" },
+  { id: 108, value: "Mr. Garrison", title: "POTUS" },
+  { id: 109, value: "Mr. Mackey", title: "M'Kay" },
+];
 
 const CustomMixedTags = () => {
   const onChange = useCallback((e) => {
@@ -30,18 +37,20 @@ const CustomMixedTags = () => {
   return (
     <>
       <MixedTags
+        whitelist={whitelist}
         autoFocus={true}
         settings={settings}
         className="myTags"
-        readOnly={false}
         onChange={onChange}
         value={`
-      type <em>@</em> and a (Latin) character.
-      <br>
-      <small>(Only tags from the <em>whitelist</em> are allowed. <em>Whitelist</em> contains names of Southpark characters.)</small
-      <br>
-      <small>(Open this demo in a full-window to be able to type new-line returns)</small>
-              `}
+This is a textarea which mixes text with [[{"value":"tags"}]].
+To add a [[{"value":"tag"}]], type <em>@</em> and a (Latin) character. Here's a [[{"value":"readonly", "readonly":true}]] tag.
+<br>
+<small>(Only tags from the <em>whitelist</em> are allowed. <em>Whitelist</em> contains names of Southpark characters.)</small
+<br>
+<small>(Open this demo in a full-window to be able to type new-line returns)</small>
+[[{ "id": 100, "value": "kenny", "title": "Kenny McCormick" }]]
+        `}
       />
     </>
   );
